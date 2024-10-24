ae41e2bdf3a31add8c2dcfee0bbcd7eabe6e343a
Wongani Chulu <wonganichulu@gmail.com>
2024-10-24 09:30:16 +0200

    Added 0-stats.py fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,3 +1,48 @@
+# #!/usr/bin/python3
+# """
+# A python script solution for alx-interview problems
+# """
+#
+# import sys
+# import re
+#
+# total_file_size = 0
+# status_codes = {}
+# line_count = 0
+# valid_status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+#
+# pattern = re.compile(
+#         r'^(\S+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
+# )
+#
+#
+# def print_stats():
+#     print("File size: {}".format(total_file_size))
+#     for code in sorted(status_codes.keys(), key=int):
+#         print("{}: {}".format(code, status_codes[code]))
+#
+#
+# try:
+#     for line in sys.stdin:
+#         line = line.strip()
+#         try:
+#             match = pattern.match(line)
+#             if match:
+#                 status_code = match.group(3)
+#                 file_size = int(match.group(4))
+#                 total_file_size += file_size
+#                 if status_code in valid_status_codes:
+#                     status_codes[status_code] = status_codes.get(
+#                             status_code, 0
+#                     ) + 1
+#                 line_count += 1
+#             if line_count % 10 == 0:
+#                 print_stats()
+#         except Exception as e:
+#             pass
+# except KeyboardInterrupt:
+#     print_stats()
+#     raise
 #!/usr/bin/python3
 """
 A python script solution for alx-interview problems
@@ -8,7 +53,6 @@ import re
 
 total_file_size = 0
 status_codes = {}
-line_count = 0
 valid_status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
 pattern = re.compile(
@@ -22,6 +66,7 @@ def print_stats():
         print("{}: {}".format(code, status_codes[code]))
 
 
+line_count = 0
 try:
     for line in sys.stdin:
         line = line.strip()
@@ -36,9 +81,9 @@ try:
                             status_code, 0
                     ) + 1
                 line_count += 1
-            if line_count % 10 == 0:
-                print_stats()
-        except Exception as e:
+                if line_count % 10 == 0:
+                    print_stats()
+        except Exception:
             pass
 except KeyboardInterrupt:
     print_stats()
